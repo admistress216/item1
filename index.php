e6a836c335768cb546c0be4572f70c1b20a1b7d5
lizengcai <lizengcai@appublisher.com>
2017-05-15 11:58:38 +0800

    test

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 $log = new Monolog\Logger('name');
-
 $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
-echo 'aa';die;
+
 $log->addWarning('Foo');
 ?>
