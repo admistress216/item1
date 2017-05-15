ac8d229aa854b040506e9d6025742f2f3608dd14
lizengcai <lizengcai@appublisher.com>
2017-05-15 11:59:35 +0800

    Revert "电子相册"
    This reverts commit 93e5f60e12a82449b921048afec6a69bc49c540e.

--- gotools/article.php
+++
@@ -1,6 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php include_once("inc/function.php") ?>
-<style>
-*{font-size:12px;color:#006699;line-height:20px;}	
-</style>
-<?php s04(); ?>
